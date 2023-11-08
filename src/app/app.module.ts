3e9b535e9b77c6090c350f8fba8b144bbef75d28
CMingrone <carlos_mingrone@hotmail.com>
2023-11-07 21:08:56 -0300

    cambios en las capetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardProductoComponent } from './components/card-producto/card-producto.component';
+import { PrincipalComponent } from './layout/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardProductoComponent
+    CardProductoComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
